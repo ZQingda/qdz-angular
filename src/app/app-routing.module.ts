9bf15b2a2a74ef1775906adfc91e3e154855a1bb
ZQingda <qingda.zou@gmail.com>
2019-05-09 19:58:43 -0400

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './containers/main/main.component';
+import { GalleryComponent } from './containers/gallery/gallery.component';
+import { ProjectsComponent } from './containers/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'photography', component: GalleryComponent },
+  { path: 'projects', component: ProjectsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './containers/gallery/gallery.component';
+import { MainComponent } from './containers/main/main.component';
+import { ProjectsComponent } from './containers/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    MainComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
